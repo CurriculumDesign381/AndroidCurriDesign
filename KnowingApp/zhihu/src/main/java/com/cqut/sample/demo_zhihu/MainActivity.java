cc528f164e59ee29d760b9af2ad190468d6566e8
unknown <2836734653@qq.com>
2018-11-07 00:58:32 +0800

    完成登录后台

--- KnowingApp/zhihu/src/main/java/com/cqut/sample/demo_zhihu/MainActivity.java
+++ KnowingApp/zhihu/src/main/java/com/cqut/sample/demo_zhihu/MainActivity.java
@@ -85,30 +85,7 @@ public class MainActivity extends SupportActivity implements BaseMainFragment.On
 
             @Override
             public void onTabReselected(int position) {
-              /*  final SupportFragment currentFragment = mFragments[position];
-                int count = currentFragment.getChildFragmentManager().getBackStackEntryCount();
 
-                // 如果不在该类别Fragment的主页,则回到主页;
-                if (count > 1) {
-                    if (currentFragment instanceof ZhihuSecondFragment) {
-                        currentFragment.popToChild(SecondHomeFragment.class, false);
-                    } else if (currentFragment instanceof ZhihuFirstFragment) {
-                        currentFragment.popToChild(ViewPagerFragment.class, false);
-                    } else if (currentFragment instanceof ColleageFragment) {
-                        currentFragment.popToChild(ColleageFragment.class, false);
-                    } else if (currentFragment instanceof HomePageFragment) {
-                        currentFragment.popToChild(MeFragment.class, false);
-                    }
-                    return;
-                }
-
-
-                // 这里推荐使用EventBus来实现 -> 解耦
-                if (count == 1) {
-                    // 在FirstPagerFragment中接收, 因为是嵌套的孙子Fragment 所以用EventBus比较方便
-                    // 主要为了交互: 重选tab 如果列表不在顶部则移动到顶部,如果已经在顶部,则刷新
-                    EventBusActivityScope.getDefault(MainActivity.this).post(new TabSelectedEvent(position));
-                }*/
             }
         });
     }
